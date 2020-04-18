bf619891acca8374f7ba3aa0cd82393b7ac3395e
blue <mlh2197@columbia.edu>
2020-04-18 16:25:51 -0400

    Finishing touches, added BundleAnalyser plugin.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
     entry: "./src/client/index.js",
@@ -29,6 +31,21 @@ module.exports = {
 		new HtmlWebPackPlugin({
 			template: "./src/client/views/index.html",
 			filename: "./index.html",
-		})
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
+        /* Creates an interactive tree-map visualization of the contents of all
+         * our bundles. */
+        new BundleAnalyzerPlugin({
+            /* Analyze an existing bundle. */
+            generateStatsFile: true
+        })
 	]
 }
