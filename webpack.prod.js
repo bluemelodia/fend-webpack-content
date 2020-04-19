48e12b0af3327fe6fac8b0f6b076ecaf3b614ab3
blue <mlh2197@columbia.edu>
2020-04-19 14:19:38 -0400

    Add configuration to output main.css file.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     entry: "./src/client/index.js",
@@ -29,7 +30,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: [ MiniCSSExtractPlugin.loader, 'css-loader', 'sass-loader' ]
             }
         ]
     },
@@ -37,6 +38,7 @@ module.exports = {
 		new HtmlWebPackPlugin({
 			template: "./src/client/views/index.html",
 			filename: "./index.html",
-		})
+        }),
+        new MiniCSSExtractPlugin({ filename: '[name].css' })
 	]
 }
